8a5ce57f4020f1950d92cffb6170cef2ee6f606c
Diyae1 <diyaeouchtine@gmail.com>
2025-06-05 01:22:51 +0100

    Version finale de mon site pour l’hébergement

--- js1.js
+++ js1.js
@@ -3,17 +3,17 @@ let inpt = document.getElementById("inpt");
 let bton = document.getElementById("bton");
 let paragraphe = document.getElementById("paragraphe");
 let lst = document.getElementById("lst");
-//clic du bouton est un evennement:
+
 /*bton.addEventListener('click', function()
 {
    if(inpt.value.trim() === "")
      {
-        paragraphe.innerHTML = "Ce champs ne peut etre vide";
+        paragraphe.innerHTML = "Le champ ne doit pas être vide";
         paragraphe.style.color = 'red';
      }
    else
      {
-       paragraphe.textContent = "Element bien saisi";
+       paragraphe.textContent = " Tâche ajoutée !";
        paragraphe.style.color = 'green';
        //ajouer une liste vide
        let valeur= document.createElement("li");
@@ -22,28 +22,37 @@ let lst = document.getElementById("lst");
        inpt.value ="";
      }  
 })
-     */
-
+*/   
+let supprimer = document.getElementById('delete');
+supprimer.style.display='none';
    inpt.addEventListener('keypress', function(event)
 {
-   if(event.key === "enter")
+   if(event.key === "Enter")
    {
        if(inpt.value.trim() === "")
      {
-        paragraphe.innerHTML = "Ce champs ne peut etre vide";
+        paragraphe.innerHTML = "Ce champ ne doit pas être vide";
         paragraphe.style.color = 'red';
      }
    else
      {
 
-       paragraphe.textContent = "Element bien saisi";
+       paragraphe.textContent = " Tâche ajoutée !";
        paragraphe.style.color = 'green';
        //ajouer une liste vide
-       let valeur= document.createElement("li");
-       valeur.textContent = inpt.value;
-       lst.appendChild(valeur);
+       let tache= document.createElement("li");
+      tache.style.listStyleType='none';
+      tache.style.backgroundColor='white';
+       let check= document.createElement("input");
+       check.type="checkbox"; 
+       tache.textContent = inpt.value;
+       tache.appendChild(check);
+       lst.appendChild(tache);
        inpt.value ="";
      } 
     } 
 })
+let bod =document.querySelector("body");
+bod.style.backgroundColor ='lightblue';
+bod.style.textAlign='center';
 
